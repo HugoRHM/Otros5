4e53f353e5af42ab1c7c3835434a0839e40e31aa
HugoRHM <72580471+HugoRHM@users.noreply.github.com>
2023-10-20 11:34:10 -0600

    Add files via upload

---
+++ Otros5/src/module-info.java
@@ -0,0 +1,2 @@
+module Otros5 {
+}
---
+++ Otros5/src/org/generation/otros5/Otros5.java
@@ -0,0 +1,37 @@
+// Hugo Ricardo Hernández Montejano 
+// Revisado por los Mictlanes
+// Califican: 5 
+
+package org.generation.otros5;
+
+import java.util.Scanner;
+
+public class Otros5 {
+	public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Introduzca un número: ");
+	        int ni = scanner.nextInt();
+	        int c = ni;
+
+	        int afo = 0;
+	        int noAfo = 0;
+	        
+	      while (ni > 0) {
+          int digito = ni % 10;
+          if (digito == 3 || digito == 7 || digito == 8 || digito == 9) {
+              afo++;
+          } else {
+              noAfo++;
+          }
+          ni /= 10;
+      }
+
+      if (afo > noAfo) {
+          System.out.println("El " + c + " es un número afortunado.");
+      } else {
+          System.out.println("El " + c + " no es un número afortunado.");
+      }
+      
+    scanner.close();
+  }
+}
